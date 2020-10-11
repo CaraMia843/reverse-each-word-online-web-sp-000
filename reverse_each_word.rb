56f09f1a72c38ac4f8659edaf59f0bdf65290666
Cara Zellner <carajeanine@gmail.com>
2020-10-11 16:26:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   array = sentence.split(" ")
   new_array = []
-  sentence.each do |sentence|
+  array.each do |sentence|
     new_array << sentence.reverse 
   end
   new_array.join(" ")
