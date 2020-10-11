84e1bfd9b94369d4e7ddede3e4472b4d28b8db58
Cara Zellner <carajeanine@gmail.com>
2020-10-11 16:19:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(sentence)
-  new_array = []
-  sentence.each do |words|
-    new_array << sentence.reverse.split(" ")
+  reversed = sentence.split(" ").collect do |word|
+    word.reverse
   end
-  new_array
+  reversed.join(" ")
 end
